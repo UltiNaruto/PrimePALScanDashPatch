1df077c3e8472c37e425afdf519bdbd9f64c1768
Ze Naruto <zenaruto@ymail.com>
2019-09-05 20:44:28 +0200

    Ajoutez des fichiers projet.

---
+++ PrimePALScanDashPatch/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PrimePALScanDashPatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Patch_btn = new System.Windows.Forms.Button();
+            this.ISOtoPatch_lbl = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.ISOtoPatch_btn = new System.Windows.Forms.Button();
+            this.ISOtoPatch_txtBox = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Patch_btn, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.ISOtoPatch_lbl, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(582, 112);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Patch_btn
+            // 
+            this.Patch_btn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Patch_btn.Location = new System.Drawing.Point(3, 83);
+            this.Patch_btn.Name = "Patch_btn";
+            this.Patch_btn.Size = new System.Drawing.Size(576, 26);
+            this.Patch_btn.TabIndex = 4;
+            this.Patch_btn.Text = "Patch";
+            this.Patch_btn.UseVisualStyleBackColor = true;
+            this.Patch_btn.Click += new System.EventHandler(this.Patch_btn_Click);
+            // 
+            // ISOtoPatch_lbl
+            // 
+            this.ISOtoPatch_lbl.AutoSize = true;
+            this.ISOtoPatch_lbl.Location = new System.Drawing.Point(3, 3);
+            this.ISOtoPatch_lbl.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.ISOtoPatch_lbl.Name = "ISOtoPatch_lbl";
+            this.ISOtoPatch_lbl.Size = new System.Drawing.Size(94, 17);
+            this.ISOtoPatch_lbl.TabIndex = 3;
+            this.ISOtoPatch_lbl.Text = "ISO to patch :";
+            this.toolTip1.SetToolTip(this.ISOtoPatch_lbl, resources.GetString("ISOtoPatch_lbl.ToolTip"));
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.Controls.Add(this.ISOtoPatch_btn, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ISOtoPatch_txtBox, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(582, 32);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // ISOtoPatch_btn
+            // 
+            this.ISOtoPatch_btn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ISOtoPatch_btn.Location = new System.Drawing.Point(545, 3);
+            this.ISOtoPatch_btn.Name = "ISOtoPatch_btn";
+            this.ISOtoPatch_btn.Size = new System.Drawing.Size(34, 26);
+            this.ISOtoPatch_btn.TabIndex = 0;
+            this.ISOtoPatch_btn.Text = "...";
+            this.ISOtoPatch_btn.UseVisualStyleBackColor = true;
+            this.ISOtoPatch_btn.Click += new System.EventHandler(this.ISOtoPatch_btn_Click);
+            // 
+            // ISOtoPatch_txtBox
+            // 
+            this.ISOtoPatch_txtBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ISOtoPatch_txtBox.Location = new System.Drawing.Point(3, 6);
+            this.ISOtoPatch_txtBox.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.ISOtoPatch_txtBox.Name = "ISOtoPatch_txtBox";
+            this.ISOtoPatch_txtBox.Size = new System.Drawing.Size(536, 22);
+            this.ISOtoPatch_txtBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 112);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 159);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 159);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Prime PAL Scan Dash Patch";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button ISOtoPatch_btn;
+        private System.Windows.Forms.TextBox ISOtoPatch_txtBox;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label ISOtoPatch_lbl;
+        private System.Windows.Forms.Button Patch_btn;
+    }
+}
+
---
+++ PrimePALScanDashPatch/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrimePALScanDashPatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            rand = new Random((int)(new DateTime().Ticks / TimeSpan.TicksPerMillisecond));
+            InitializeComponent();
+        }
+
+        private void ISOtoPatch_btn_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "GC ISO File|*.iso";
+                openFileDialog.FileName = "";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.ISOtoPatch_txtBox.Text = openFileDialog.SafeFileName;
+                }
+            }
+        }
+
+        private void Patch_btn_Click(object sender, EventArgs e)
+        {
+            using (var file = File.Open(this.ISOtoPatch_txtBox.Text, FileMode.Open))
+            using (var BW = new BinaryWriter(file))
+            using (var BR = new BinaryReader(file))
+            {
+                file.Seek(0x31FB2C8, SeekOrigin.Begin);
+                ulong bytesToPatch = BitConverter.ToUInt32(BR.ReadBytes(4), 0);
+                if (bytesToPatch == 0x10008241)
+                {
+                    MessageBox.Show("Unknown version used as source ISO");
+                    return;
+                }
+                if (bytesToPatch == 0x18000048)
+                {
+                    MessageBox.Show("ISO is already patched");
+                    return;
+                }
+                file.Seek(0x31FB2C8, SeekOrigin.Begin);
+                BW.Write(0x18000048);
+            }
+            MessageBox.Show("Scan dash is now enabled for this ISO");
+        }
+    }
+}
